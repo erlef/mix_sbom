f11b39471fb115bc5293719064218f6441d259d6
Jonatan Männchen <jonatan@maennchen.ch>
2025-11-27 16:19:17 +0100

    Expose Full OTP Purl

---
+++ test/sbom/scm/mix/scm/git_test.exs
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: 2025 Erlang Ecosystem Foundation
+
+defmodule SBoM.SCM.Mix.SCM.GitTest do
+  use SBoM.FixtureCase, async: false
+
+  alias SBoM.SCM.Mix.SCM.Git
+
+  doctest Git
+end
---
+++ test/sbom/scm_test.exs
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: 2025 Erlang Ecosystem Foundation
+
+defmodule SBoM.SCMTest do
+  use SBoM.FixtureCase, async: false
+
+  alias SBoM.SCM
+
+  doctest SCM
+end
