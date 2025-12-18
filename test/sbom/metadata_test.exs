54500ba5e56a80c51dee123e32dca899c9eeaa2b
Barbara Chassoul <barbara.chassoul@stritzinger.com>
2025-12-18 01:33:33 -0600

    Add SPDX Tags (#71)

--- test/sbom/metadata_test.exs
+++ test/sbom/metadata_test.exs
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # SPDX-FileCopyrightText: 2025 Erlang Ecosystem Foundation
+# SPDX-FileCopyrightText: 2025 Stritzinger GmbH
 
 defmodule SBoM.MetadataTest do
   use ExUnit.Case, async: true
